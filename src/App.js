f09622971051706f57215a6e918a7670385354eb
wjddns7828 <wjddns7828>
2024-08-22 23:09:43 +0900

    초기 커밋 메시지

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  const name = '리액트';
+  return (
+    <div>
+      <h1>{name} 안녕</h1>
+      <h2>잘 작동하니?</h2>
+    </div>
+  );
+}
+
+export default App;
